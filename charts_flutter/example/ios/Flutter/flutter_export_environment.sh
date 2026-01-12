947e37b42411249303be6e1af2e820325c933d5f
Jonas_Santos <163153811+JonasHahndosSantos@users.noreply.github.com>
2026-01-12 10:53:03 -0300

    ajustes realizados

--- charts_flutter/example/ios/Flutter/flutter_export_environment.sh
+++ charts_flutter/example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\evald\fvm\default"
-export "FLUTTER_APPLICATION_PATH=C:\Users\evald\Documents\Cervantes\projetos\charts_flutter\charts_flutter\example"
+export "FLUTTER_ROOT=C:\Users\desen\fvm\default"
+export "FLUTTER_APPLICATION_PATH=C:\Users\desen\Documents\GitHub\charts_flutter\charts_flutter\example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
